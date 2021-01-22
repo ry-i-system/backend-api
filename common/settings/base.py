a8b6a9718b7fae4e8b490cb036a11ff1899ded5b
Ryo Yasumoto <ryo.yasumoto@ry-i-system.com>
2021-01-22 18:30:54 +0900

    commit

--- common/settings/base.py
+++ common/settings/base.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'rest_auth',
     'rest_auth.registration',
     'corsheaders',
